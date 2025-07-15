fe5bc4bc8a6ae461ddbd9ca51ab8e75747a24615
Abdullah Anwar <abdullah860259@gmail.com>
2025-07-15 14:37:57 -0700

    Added lInks

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -8,14 +8,26 @@ const Links = () => {
     return (
         <>
             <div className="links -translate-y-1 mr-4 md:mr-10 flex gap-5 relative z-20 text-2xl text-gray-600 flex-col ml-auto " >
-                <GrLinkedinOption  className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
-                <FaTwitter  className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
-                <FaInstagram  className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
-                <MdOutlineMail  className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
-                <FaGithub  className='sm:w-[37px] hover:text-[#fff] pointer-events-auto  hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                <a href="https://www.linkedin.com/in/abdullah-anwar-a4013633a/" target="_blank" rel="noopener noreferrer">
+                    <GrLinkedinOption className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                </a>
+                <a href="https://x.com/Abdulla55799774" target="_blank" rel="noopener noreferrer">
+                <FaTwitter className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                </a>
+                <a href="https://www.instagram.com/abdu_7817/" target="_blank" rel="noopener noreferrer" >
+                    <FaInstagram className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                </a>
+                <a href="mailto:abdullah860259@gmail.com">
+                <MdOutlineMail className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                </a>
+                <a href="https://github.com/Abdullah860259" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className='sm:w-[37px] hover:text-[#fff] pointer-events-auto  hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                </a>
             </div>
         </>
     )
 }
 
 export default Links
+
+
