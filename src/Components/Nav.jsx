01855214744ea1d1cde6a524a8c96502b3a9fe26
Abdullah Anwar <abdullah860259@gmail.com>
2025-07-17 20:13:40 -0700

    added the animation to profile

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,7 +7,7 @@ export const Nav = React.forwardRef((props, ref) => {
   return (
     <>
       <div ref={ref} className='Nav transition-opacity ease-in-out duration-100 delay-500 pointer-events-none w-full px-6 py-3 md:px-16 max-w-screen-xl flex justify-between items-center absolute top-0'>
-        <svg className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 sm:w-[70px] sm:h-[70px] w-14 h-14  transition-opacity ease-in-out  ' onClick={() => window.location.reload()} viewBox="0 0 406 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 sm:w-[70px] sm:h-[70px] w-14 h-14  transition-opacity ease-in-out  ' onClick={() => window.location.href = "/"} viewBox="0 0 406 368" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M314.516 203.174L314.516 349.104L162.117 208.727M314.516 203.174L282.653 203.26M314.516 203.174L337.498 203.26L380.617 170.782L314.516 170.782M162.117 208.727L117.661 170.634L157.117 170.782M162.117 208.727L65.1165 301.139L20.176 301.139L137.099 188.83M200.117 170.782L282.653 96.1739L282.653 170.782M200.117 170.782L282.653 170.782M200.117 170.782L157.117 170.782M282.653 203.26L210.462 202.796L282.653 270.76L282.653 203.26ZM282.653 170.782L314.516 170.782M157.117 170.782L315.751 20.1061L314.516 170.782" stroke="white" strokeWidth="10" />
         </svg>
         <ul
