b73e3cfaca690778e3e456c469f37a9f4ceb2638
Abdullah Anwar <abdullah860259@gmail.com>
2025-07-17 21:21:18 -0700

    added the overall feature

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
                         <h1 className=" text-white font-bold leading-tight text-[24px] sm:text-[40px] md:text-[50px] lg:text-[64px] font-spartan pointer-events-none ">Hafiz Abdullah Anwar</h1>
                         <p className="text-white font-bold italic font-['Merriweather'] text-[12px] sm:text-[18px] md:text-[16px] lg:text-[16px] mt-3 mb-8 pointer-events-none" >MERN Stack Developer</p>
                         <button
-                            className='about-btn cursor-pointer pointer-events-auto bg-blue-500 font-poppins font-bold text-[8px] sm:text-[16px] sm:font-extrabold bg-gradient-to-l from-[#1595b6] scale-100 to-[#1f2667e6] text-white sm:py-2 sm:px-5 rounded-lg hover:scale-110 transition-scale duration-100 ease-in-out px-3 py-[6px] '
+                            className='about-btn cursor-pointer pointer-events-auto bg-blue-500 font-poppins font-bold text-[8px] sm:text-[16px] sm:font-extrabold bg-gradient-to-l from-[#1595b6] scale-100 to-[#1f2667e6] text-white sm:py-2 sm:px-5 rounded-lg hover:scale-110 transition-scale duration-100 ease-in-out px-3 py-[6px] active:scale-90    '
                             onClick={() => {
                                 setshowAboutMe(!showAboutMe),
                                 navAndDisc.current?.classList.toggle("navAndDiscHide")
@@ -55,7 +55,7 @@ const Home = () => {
                         transition={{ duration: 0.8 }}    // Duration in seconds
                         className="bg-logo pointer-events-none"></motion.div>
                     <button
-                        className='latestprojects-btn cursor-pointer relative top-28 bg-blue-500 font-poppins font-bold text-[8px] sm:text-[16px] sm:font-extrabold bg-gradient-to-l from-[#1595b6] scale-100 to-[#1f2667e6] text-white sm:py-2 sm:px-5 rounded-lg hover:scale-110 transition-scale duration-100 ease-in-out px-3 py-[6px]  ' >Latest Works</button>
+                        className='latestprojects-btn cursor-pointer relative top-28 bg-blue-500 font-poppins font-bold text-[8px] sm:text-[16px] sm:font-extrabold bg-gradient-to-l from-[#1595b6] scale-100 active:scale-90 to-[#1f2667e6] text-white sm:py-2 sm:px-5 rounded-lg hover:scale-110 transition-scale duration-100 ease-in-out px-3 py-[6px]  ' >Latest Works</button>
                 </section >
             </AnimatePresence>
             <AnimatePresence>
--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -9,19 +9,19 @@ const Links = React.forwardRef((props,ref) => {
         <>
             <div ref={ref} className="links -translate-y-1 mr-4 md:mr-10 flex gap-5 relative z-20 text-2xl text-gray-600 flex-col ml-auto " >
                 <a href="https://www.linkedin.com/in/abdullah-anwar-a4013633a/" target="_blank" rel="noopener noreferrer">
-                    <GrLinkedinOption className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                    <GrLinkedinOption className='  active:scale-90 pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
                 </a>
                 <a href="https://x.com/Abdulla55799774" target="_blank" rel="noopener noreferrer">
-                <FaTwitter className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                <FaTwitter className='  active:scale-90 pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
                 </a>
                 <a href="https://www.instagram.com/abdu_7817/" target="_blank" rel="noopener noreferrer" >
-                    <FaInstagram className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                    <FaInstagram className='  active:scale-90 pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
                 </a>
                 <a href="mailto:abdullah860259@gmail.com">
-                <MdOutlineMail className=' pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                <MdOutlineMail className='  active:scale-90 pointer-events-auto sm:w-[37px] hover:text-[#fff] hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
                 </a>
                 <a href="https://github.com/Abdullah860259" target="_blank" rel="noopener noreferrer">
-                    <FaGithub className='sm:w-[37px] hover:text-[#fff] pointer-events-auto  hover:scale-125 cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
+                    <FaGithub className='sm:w-[37px] hover:text-[#fff] pointer-events-auto  hover:scale-125 active:scale-90  cursor-pointer transition-scale duration-150 text-[#c3c5d4] h-auto ' />
                 </a>
             </div>
         </>
