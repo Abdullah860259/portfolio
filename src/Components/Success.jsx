f9b7cf3fd7311f775fa777150a4f100f11748016
Abdullah Anwar <abdullah860259@gmail.com>
2025-07-29 14:49:10 +0500

    added

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -52,7 +52,10 @@ const Error = (props) => {
                     <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 412L346-160H160v-186L28-480l132-134v-186h186l134-132 134 132h186v186l132 134-132 134v186H614L480-28Zm0-112 100-100h140v-140l100-100-100-100v-140H580L480-820 380-720H240v140L140-480l100 100v140h140l100 100Zm0-340Z" /></svg>
                 </div> */}
                 <p >{props.decs}</p>
-                <svg onClick={() => setExiting(true)} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#9DC384"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg>
+                <div className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 active:scale-90 transition ease-in-out'>
+                    <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setExiting(true)} className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 active:scale-90 transition ease-in-out' width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                </div>
+                {/* <svg  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#9DC384"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg> */}
             </div>
         </>
     )
