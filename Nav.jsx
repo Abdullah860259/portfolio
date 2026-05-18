78ae2094e7867756b52d6ec0ccb6a0c3cecef896
Hafiz Abdullah Anwer <169032711+Abdullah860259@users.noreply.github.com>
2026-05-18 13:27:21 +0500

    Add files via upload

---
+++ Nav.jsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from 'react'
+import { IoClose } from "react-icons/io5";
+import "./nav.css"
+import { Link } from 'react-router-dom';
+import { AnimatePresence, motion } from "framer-motion";
+
+
+
+export const Nav = React.forwardRef((props, ref) => {
+  const [showNav, setShowNav] = useState(props.showNav);
+  const [showNavColomn, setshowNavColomn] = useState(false);
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'; // disable scroll on mount
+    return () => {
+      document.body.style.overflow = 'auto'; // enable scroll on unmount
+    };
+  }, []);
+  return (
+    <>
+      <div ref={ref} className='Nav transition-opacity   ease-in-out duration-100 delay-500 pointer-events-none w-full px-6 py-3 md:px-16 max-w-screen-xl flex justify-between items-center absolute top-0'>
+        <svg className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 sm:w-[70px] sm:h-[70px] w-14 h-14 active:scale-90 transition-opacity ease-in-out  ' onClick={() => window.location.href = "/"} viewBox="0 0 406 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M314.516 203.174L314.516 349.104L162.117 208.727M314.516 203.174L282.653 203.26M314.516 203.174L337.498 203.26L380.617 170.782L314.516 170.782M162.117 208.727L117.661 170.634L157.117 170.782M162.117 208.727L65.1165 301.139L20.176 301.139L137.099 188.83M200.117 170.782L282.653 96.1739L282.653 170.782M200.117 170.782L282.653 170.782M200.117 170.782L157.117 170.782M282.653 203.26L210.462 202.796L282.653 270.76L282.653 203.26ZM282.653 170.782L314.516 170.782M157.117 170.782L315.751 20.1061L314.516 170.782" stroke="white" strokeWidth="10" />
+        </svg>
+        <ul
+          className='hidden nav-links select-none sm:flex gap-5 md:gap-10  text-[#c3c5d4] text-[15px] sm:text-xl pointer-events-auto'
+        >
+          {props.home ? (
+            <Link className='overflow-visible' to="/profile">
+              <motion.li
+                whileTap={{ scale: 0.8 }}
+                transition={{ duration: 0.05 }}
+                className="hover:cursor-pointer pt-3 pl-3 pr-3  hover:text-white w-full h-full overflow-visible transition-colors duration-200 hover:scale-105"
+              >
+                Profile
+              </motion.li>
+            </Link>
+          ) : (
+            <Link to="/">
+              <motion.li
+                whileTap={{ scale: 0.8 }}
+                transition={{ duration: 0.05 }}
+                className="hover:cursor-pointer pt-3 pl-3 pr-3  hover:text-white transition-colors duration-200 hover:scale-105"
+              >
+                Back to Home
+              </motion.li>
+            </Link>
+          )}
+          {props.home ? (
+
+            <div>
+              <motion.li
+                whileTap={{ scale: 0.8 }}
+                transition={{ duration: 0.05 }}
+                onClick={() => {
+                  props.showContact();
+                }}
+                className='hover:cursor-pointer pt-3 pl-3 pr-3  hover:text-white transition-colors duration-200 hover:scale-105 transition-scale   ' >Contact</motion.li></div>
+          ) : null}
+          {props.home ? (
+
+            <div>
+              <Link to={"/net-experience"} >
+                <motion.li
+                  whileTap={{ scale: 0.8 }}
+                  transition={{ duration: 0.05 }}
+                  className='hover:cursor-pointer pt-3 pl-3 pr-3  hover:text-white transition-colors duration-200 hover:scale-105 transition-scale   ' >NET Experience</motion.li>
+              </Link>
+            </div>
+          ) : null}
+        </ul>
+        {showNav ? (
+          <div className='flex justify-center items-center gap-4' >
+            <a href="https://wa.me/923101733247?text=Hello%2C%20I'm%20interested%20in%20your%20services%21" target="_blank" rel="noopener noreferrer" aria-label="Chat with Abdullah Anwar on WhatsApp"
+
+            >
+              <svg className=' active:scale-90 hover:scale-125 pointer-events-auto cursor-pointer hover:opacity-80 duration-150 transition ease-in-out w-[30px] h-[30px]' fill="white" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30.667 30.667" xml:space="preserve" stroke="white"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M30.667,14.939c0,8.25-6.74,14.938-15.056,14.938c-2.639,0-5.118-0.675-7.276-1.857L0,30.667l2.717-8.017 c-1.37-2.25-2.159-4.892-2.159-7.712C0.559,6.688,7.297,0,15.613,0C23.928,0.002,30.667,6.689,30.667,14.939z M15.61,2.382 c-6.979,0-12.656,5.634-12.656,12.56c0,2.748,0.896,5.292,2.411,7.362l-1.58,4.663l4.862-1.545c2,1.312,4.393,2.076,6.963,2.076 c6.979,0,12.658-5.633,12.658-12.559C28.27,8.016,22.59,2.382,15.61,2.382z M23.214,18.38c-0.094-0.151-0.34-0.243-0.708-0.427 c-0.367-0.184-2.184-1.069-2.521-1.189c-0.34-0.123-0.586-0.185-0.832,0.182c-0.243,0.367-0.951,1.191-1.168,1.437 c-0.215,0.245-0.43,0.276-0.799,0.095c-0.369-0.186-1.559-0.57-2.969-1.817c-1.097-0.972-1.838-2.169-2.052-2.536 c-0.217-0.366-0.022-0.564,0.161-0.746c0.165-0.165,0.369-0.428,0.554-0.643c0.185-0.213,0.246-0.364,0.369-0.609 c0.121-0.245,0.06-0.458-0.031-0.643c-0.092-0.184-0.829-1.984-1.138-2.717c-0.307-0.732-0.614-0.611-0.83-0.611 c-0.215,0-0.461-0.03-0.707-0.03S9.897,8.215,9.56,8.582s-1.291,1.252-1.291,3.054c0,1.804,1.321,3.543,1.506,3.787 c0.186,0.243,2.554,4.062,6.305,5.528c3.753,1.465,3.753,0.976,4.429,0.914c0.678-0.062,2.184-0.885,2.49-1.739 C23.307,19.268,23.307,18.533,23.214,18.38z"></path> </g> </g></svg>
+            </a>
+            <a href="tel:923101733247" aria-label="Call Abdullah Anwar">
+              <svg fill="white" className=' active:scale-90 hover:scale-125  pointer-events-auto cursor-pointer hover:opacity-80 duration-150 transition ease-in-out w-[30px] h-[30px]' height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 473.806 473.806" xml:space="preserve" stroke="white"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M374.456,293.506c-9.7-10.1-21.4-15.5-33.8-15.5c-12.3,0-24.1,5.3-34.2,15.4l-31.6,31.5c-2.6-1.4-5.2-2.7-7.7-4 c-3.6-1.8-7-3.5-9.9-5.3c-29.6-18.8-56.5-43.3-82.3-75c-12.5-15.8-20.9-29.1-27-42.6c8.2-7.5,15.8-15.3,23.2-22.8 c2.8-2.8,5.6-5.7,8.4-8.5c21-21,21-48.2,0-69.2l-27.3-27.3c-3.1-3.1-6.3-6.3-9.3-9.5c-6-6.2-12.3-12.6-18.8-18.6 c-9.7-9.6-21.3-14.7-33.5-14.7s-24,5.1-34,14.7c-0.1,0.1-0.1,0.1-0.2,0.2l-34,34.3c-12.8,12.8-20.1,28.4-21.7,46.5 c-2.4,29.2,6.2,56.4,12.8,74.2c16.2,43.7,40.4,84.2,76.5,127.6c43.8,52.3,96.5,93.6,156.7,122.7c23,10.9,53.7,23.8,88,26 c2.1,0.1,4.3,0.2,6.3,0.2c23.1,0,42.5-8.3,57.7-24.8c0.1-0.2,0.3-0.3,0.4-0.5c5.2-6.3,11.2-12,17.5-18.1c4.3-4.1,8.7-8.4,13-12.9 c9.9-10.3,15.1-22.3,15.1-34.6c0-12.4-5.3-24.3-15.4-34.3L374.456,293.506z M410.256,398.806 C410.156,398.806,410.156,398.906,410.256,398.806c-3.9,4.2-7.9,8-12.2,12.2c-6.5,6.2-13.1,12.7-19.3,20 c-10.1,10.8-22,15.9-37.6,15.9c-1.5,0-3.1,0-4.6-0.1c-29.7-1.9-57.3-13.5-78-23.4c-56.6-27.4-106.3-66.3-147.6-115.6 c-34.1-41.1-56.9-79.1-72-119.9c-9.3-24.9-12.7-44.3-11.2-62.6c1-11.7,5.5-21.4,13.8-29.7l34.1-34.1c4.9-4.6,10.1-7.1,15.2-7.1 c6.3,0,11.4,3.8,14.6,7c0.1,0.1,0.2,0.2,0.3,0.3c6.1,5.7,11.9,11.6,18,17.9c3.1,3.2,6.3,6.4,9.5,9.7l27.3,27.3 c10.6,10.6,10.6,20.4,0,31c-2.9,2.9-5.7,5.8-8.6,8.6c-8.4,8.6-16.4,16.6-25.1,24.4c-0.2,0.2-0.4,0.3-0.5,0.5 c-8.6,8.6-7,17-5.2,22.7c0.1,0.3,0.2,0.6,0.3,0.9c7.1,17.2,17.1,33.4,32.3,52.7l0.1,0.1c27.6,34,56.7,60.5,88.8,80.8 c4.1,2.6,8.3,4.7,12.3,6.7c3.6,1.8,7,3.5,9.9,5.3c0.4,0.2,0.8,0.5,1.2,0.7c3.4,1.7,6.6,2.5,9.9,2.5c8.3,0,13.5-5.2,15.2-6.9 l34.2-34.2c3.4-3.4,8.8-7.5,15.1-7.5c6.2,0,11.3,3.9,14.4,7.3c0.1,0.1,0.1,0.1,0.2,0.2l55.1,55.1 C420.456,377.706,420.456,388.206,410.256,398.806z"></path> <path d="M256.056,112.706c26.2,4.4,50,16.8,69,35.8s31.3,42.8,35.8,69c1.1,6.6,6.8,11.2,13.3,11.2c0.8,0,1.5-0.1,2.3-0.2 c7.4-1.2,12.3-8.2,11.1-15.6c-5.4-31.7-20.4-60.6-43.3-83.5s-51.8-37.9-83.5-43.3c-7.4-1.2-14.3,3.7-15.6,11 S248.656,111.506,256.056,112.706z"></path> <path d="M473.256,209.006c-8.9-52.2-33.5-99.7-71.3-137.5s-85.3-62.4-137.5-71.3c-7.3-1.3-14.2,3.7-15.5,11 c-1.2,7.4,3.7,14.3,11.1,15.6c46.6,7.9,89.1,30,122.9,63.7c33.8,33.8,55.8,76.3,63.7,122.9c1.1,6.6,6.8,11.2,13.3,11.2 c0.8,0,1.5-0.1,2.3-0.2C469.556,223.306,474.556,216.306,473.256,209.006z"></path> </g> </g> </g></svg>
+            </a>
+          </div>
+        ) :
+          (<svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#FFFFFF"
+            className='cursor-pointer pointer-events-auto hover:opacity-80 duration-150 active:scale-90 transition ease-in-out'
+            onClick={() => {
+              setshowNavColomn(true);
+              document.body.style.overflow = 'hidden';
+            }}
+          >
+            <path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z" />
+          </svg>)}
+        <AnimatePresence>
+          {showNavColomn && (
+            <motion.div
+              className="absolute  pointer-events-auto top-0 right-0 w-1/2 h-[100dvh] z-50 bg-[rgb(4,5,24)] flex flex-col items-center justify-start py-4"
+              initial={{ x: "100%" }}
+              animate={{ x: showNavColomn ? 0 : "100%" }}
+              transition={{ duration: 0.2 }}
+              exit={{ x: "100%" }}
+            >
+              <IoClose
+                className="text-2xl text-white self-end m-4 cursor-pointer"
+                onClick={() => {
+                  setshowNavColomn(false);
+                  document.body.style.overflow = 'auto';
+                }}
+              />
+              <ul className='flex flex-col gap-5 text-white w-full mt-10'>
+                <li className='w-full ' ><Link to="/"
+                  className="  p-5 rounded-sm block active:scale-90 w-full  transition active:bg-[#1784ab] hover:cursor-pointer hover:text-white  duration-200 hover:scale-105 "
+                  onClick={() => {
+                    setshowNavColomn(false);
+                    document.body.style.overflow = 'auto';
+                  }}
+                >Home</Link> </li>
+                <li className='w-full' ><Link to="/profile" className="  p-5 rounded-sm block active:scale-90 w-full transition active:bg-[#1784ab]  hover:cursor-pointer hover:text-white  duration-200 hover:scale-105 " >Profile</Link> </li>
+                {props.home ? (
+                  <li
+                    onClick={() => {
+                      props.showContact();
+                      setshowNavColomn(false);
+                    }}
+                    className='w-full p-5 rounded-sm block active:scale-90 w-fulltransition active:bg-[#1784ab]  hover:cursor-pointer hover:text-white duration-200 hover:scale-105 ' >Contact </li>
+                ) : null}
+                {props.home ? (
+                  <li
+                    onClick={() => {
+
+                      setshowNavColomn(false);
+                    }}
+                    className='w-full p-5 rounded-sm block active:scale-90 w-fulltransition active:bg-[#1784ab]  hover:cursor-pointer hover:text-white duration-200 hover:scale-105 ' >
+                    <Link to={"/net-experience"} >
+                      NET Experience
+                    </Link> </li>
+                ) : null}
+                <div className='w-full p-5 flex justify-between items-center'>
+                  <div className='flex justify-center items-center gap-4' >
+                    <a href="https://wa.me/923101733247?text=Hello%2C%20I'm%20interested%20in%20your%20services%21
+" target="_blank" rel="noopener noreferrer" aria-label="Chat with Abdullah Anwar on WhatsApp"
+
+                    >
+                      <svg className=' active:scale-90 hover:scale-125 pointer-events-auto cursor-pointer hover:opacity-80 duration-150 transition ease-in-out w-[20px] h-[20px]' fill="white" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30.667 30.667" xml:space="preserve" stroke="white"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M30.667,14.939c0,8.25-6.74,14.938-15.056,14.938c-2.639,0-5.118-0.675-7.276-1.857L0,30.667l2.717-8.017 c-1.37-2.25-2.159-4.892-2.159-7.712C0.559,6.688,7.297,0,15.613,0C23.928,0.002,30.667,6.689,30.667,14.939z M15.61,2.382 c-6.979,0-12.656,5.634-12.656,12.56c0,2.748,0.896,5.292,2.411,7.362l-1.58,4.663l4.862-1.545c2,1.312,4.393,2.076,6.963,2.076 c6.979,0,12.658-5.633,12.658-12.559C28.27,8.016,22.59,2.382,15.61,2.382z M23.214,18.38c-0.094-0.151-0.34-0.243-0.708-0.427 c-0.367-0.184-2.184-1.069-2.521-1.189c-0.34-0.123-0.586-0.185-0.832,0.182c-0.243,0.367-0.951,1.191-1.168,1.437 c-0.215,0.245-0.43,0.276-0.799,0.095c-0.369-0.186-1.559-0.57-2.969-1.817c-1.097-0.972-1.838-2.169-2.052-2.536 c-0.217-0.366-0.022-0.564,0.161-0.746c0.165-0.165,0.369-0.428,0.554-0.643c0.185-0.213,0.246-0.364,0.369-0.609 c0.121-0.245,0.06-0.458-0.031-0.643c-0.092-0.184-0.829-1.984-1.138-2.717c-0.307-0.732-0.614-0.611-0.83-0.611 c-0.215,0-0.461-0.03-0.707-0.03S9.897,8.215,9.56,8.582s-1.291,1.252-1.291,3.054c0,1.804,1.321,3.543,1.506,3.787 c0.186,0.243,2.554,4.062,6.305,5.528c3.753,1.465,3.753,0.976,4.429,0.914c0.678-0.062,2.184-0.885,2.49-1.739 C23.307,19.268,23.307,18.533,23.214,18.38z"></path> </g> </g></svg>
+                    </a>
+                    <a href="tel:923101733247" aria-label="Call Abdullah Anwar"
+                    >
+                      <svg fill="white" className=' active:scale-90 hover:scale-125  pointer-events-auto cursor-pointer hover:opacity-80 duration-150 transition ease-in-out w-[20px] h-[20px]' height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 473.806 473.806" xml:space="preserve" stroke="white"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M374.456,293.506c-9.7-10.1-21.4-15.5-33.8-15.5c-12.3,0-24.1,5.3-34.2,15.4l-31.6,31.5c-2.6-1.4-5.2-2.7-7.7-4 c-3.6-1.8-7-3.5-9.9-5.3c-29.6-18.8-56.5-43.3-82.3-75c-12.5-15.8-20.9-29.1-27-42.6c8.2-7.5,15.8-15.3,23.2-22.8 c2.8-2.8,5.6-5.7,8.4-8.5c21-21,21-48.2,0-69.2l-27.3-27.3c-3.1-3.1-6.3-6.3-9.3-9.5c-6-6.2-12.3-12.6-18.8-18.6 c-9.7-9.6-21.3-14.7-33.5-14.7s-24,5.1-34,14.7c-0.1,0.1-0.1,0.1-0.2,0.2l-34,34.3c-12.8,12.8-20.1,28.4-21.7,46.5 c-2.4,29.2,6.2,56.4,12.8,74.2c16.2,43.7,40.4,84.2,76.5,127.6c43.8,52.3,96.5,93.6,156.7,122.7c23,10.9,53.7,23.8,88,26 c2.1,0.1,4.3,0.2,6.3,0.2c23.1,0,42.5-8.3,57.7-24.8c0.1-0.2,0.3-0.3,0.4-0.5c5.2-6.3,11.2-12,17.5-18.1c4.3-4.1,8.7-8.4,13-12.9 c9.9-10.3,15.1-22.3,15.1-34.6c0-12.4-5.3-24.3-15.4-34.3L374.456,293.506z M410.256,398.806 C410.156,398.806,410.156,398.906,410.256,398.806c-3.9,4.2-7.9,8-12.2,12.2c-6.5,6.2-13.1,12.7-19.3,20 c-10.1,10.8-22,15.9-37.6,15.9c-1.5,0-3.1,0-4.6-0.1c-29.7-1.9-57.3-13.5-78-23.4c-56.6-27.4-106.3-66.3-147.6-115.6 c-34.1-41.1-56.9-79.1-72-119.9c-9.3-24.9-12.7-44.3-11.2-62.6c1-11.7,5.5-21.4,13.8-29.7l34.1-34.1c4.9-4.6,10.1-7.1,15.2-7.1 c6.3,0,11.4,3.8,14.6,7c0.1,0.1,0.2,0.2,0.3,0.3c6.1,5.7,11.9,11.6,18,17.9c3.1,3.2,6.3,6.4,9.5,9.7l27.3,27.3 c10.6,10.6,10.6,20.4,0,31c-2.9,2.9-5.7,5.8-8.6,8.6c-8.4,8.6-16.4,16.6-25.1,24.4c-0.2,0.2-0.4,0.3-0.5,0.5 c-8.6,8.6-7,17-5.2,22.7c0.1,0.3,0.2,0.6,0.3,0.9c7.1,17.2,17.1,33.4,32.3,52.7l0.1,0.1c27.6,34,56.7,60.5,88.8,80.8 c4.1,2.6,8.3,4.7,12.3,6.7c3.6,1.8,7,3.5,9.9,5.3c0.4,0.2,0.8,0.5,1.2,0.7c3.4,1.7,6.6,2.5,9.9,2.5c8.3,0,13.5-5.2,15.2-6.9 l34.2-34.2c3.4-3.4,8.8-7.5,15.1-7.5c6.2,0,11.3,3.9,14.4,7.3c0.1,0.1,0.1,0.1,0.2,0.2l55.1,55.1 C420.456,377.706,420.456,388.206,410.256,398.806z"></path> <path d="M256.056,112.706c26.2,4.4,50,16.8,69,35.8s31.3,42.8,35.8,69c1.1,6.6,6.8,11.2,13.3,11.2c0.8,0,1.5-0.1,2.3-0.2 c7.4-1.2,12.3-8.2,11.1-15.6c-5.4-31.7-20.4-60.6-43.3-83.5s-51.8-37.9-83.5-43.3c-7.4-1.2-14.3,3.7-15.6,11 S248.656,111.506,256.056,112.706z"></path> <path d="M473.256,209.006c-8.9-52.2-33.5-99.7-71.3-137.5s-85.3-62.4-137.5-71.3c-7.3-1.3-14.2,3.7-15.5,11 c-1.2,7.4,3.7,14.3,11.1,15.6c46.6,7.9,89.1,30,122.9,63.7c33.8,33.8,55.8,76.3,63.7,122.9c1.1,6.6,6.8,11.2,13.3,11.2 c0.8,0,1.5-0.1,2.3-0.2C469.556,223.306,474.556,216.306,473.256,209.006z"></path> </g> </g> </g></svg>
+                    </a>
+                  </div></div>
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
+  )
+})
+
+export default Nav
